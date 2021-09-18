dea9ac6fa6eceaad59a6ce2486c6d14c205f05ce
ruofan <tinawei@damaiapp.com.tw>
2021-09-18 09:33:36 +0800

    update script in package.json

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import { defineConfig } from "vite"
+import { svelte } from "@sveltejs/vite-plugin-svelte"
+import sveltePreprocess from "svelte-preprocess"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,4 +13,4 @@ export default defineConfig({
   server: {
     port: 5000,
   },
-});
+})
